ed23f7a6ba182d5273c1cba2f3609659724c9853
Pranit Dakhinkar <97027846+dakhinkar@users.noreply.github.com>
2022-12-11 12:34:34 +0530

    Update script.js
    toggle theame

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+const app = document.getElementById('app');
+const swapButton = document.getElementById('swap');
+
+const swapTheame = function(e) {
+    if (this.className == 'button_day') {
+        // console.log("day");
+        app.classList.remove('day');
+        app.classList.add('night');
+        swapButton.classList.remove('button_day');
+        swapButton.classList.add('button_night');
+    } else {
+        app.classList.remove('night');
+        app.classList.add('day');
+        swapButton.classList.remove('button_night');
+        swapButton.classList.add('button_day');
+  
+    }
+}
+
+swapButton.addEventListener('click', swapTheame);
